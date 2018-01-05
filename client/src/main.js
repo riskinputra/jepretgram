91c80d308202c7ba89522cc4c9cedd55541a1537
riskinputra <riski.n.putra@gmail.com>
2018-01-05 13:09:05 +0700

    initial commit client

---
+++ client/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { store } from './vuex/store'
+import Vuetify from 'vuetify'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import('vuetify/dist/vuetify.min.css')
+
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAFZ44PGwSAWU9RAu7xpuToJHJjIPhl8Is',
+    libraries: 'places'
+  }
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store: store,
+  template: '<App/>',
+  components: { App }
+})
