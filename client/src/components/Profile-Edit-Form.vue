33b0a6e22f47eac82d082e42a035201b84d4c6ab
riskinputra <riski.n.putra@gmail.com>
2018-01-07 02:30:36 +0700

    edit profifile 1

--- client/src/components/Profile-Edit-Form.vue
+++ client/src/components/Profile-Edit-Form.vue
@@ -9,13 +9,14 @@
       <span>Add Post Successful!</span>
       <v-icon dark>done_all</v-icon>
     </v-snackbar>
-    <v-form @submit.prevent="addPost(form)" ref="form">
+    <v-form @submit.prevent="editProfile(form)" ref="form">
       <v-container grid-list-xl fluid>
         <v-layout wrap>
           <v-flex xs12>
             <VueImgInputer 
             v-model="form.image"
-            :rules="rules.address" 
+            :rules="rules.image"
+            :imgSrc="profileAccount.image"
             theme="light" style="width:100%; height:200px;"
             placeholder="Upload Image"
             bottomText="This Your Image">
@@ -24,15 +25,33 @@
           <v-flex xs12>
             <v-text-field
               color="blue darken-2"
+              label="Username"
               required
-              multi-line
-              v-model="form.description"
-              :rules="rules.description"
-            >
-              <div slot="label">
-                Description*
-              </div>
-            </v-text-field>
+              v-model="form.username"
+              :rules="rules.username"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field
+              color="blue darken-2"
+              label="Email"
+              required
+              :value="profileAccount.email"
+              v-model="form.email"
+              :rules="rules.emailRules"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field
+              color="blue darken-2"
+              label="Password"
+              required
+              v-model="form.password"
+              :rules="rules.password"
+              :append-icon="e1 ? 'visibility' : 'visibility_off'"
+              :append-icon-cb="() => (e1 = !e1)"
+              :type="e1 ? 'password' : 'text'"
+            ></v-text-field>
           </v-flex>
         </v-layout>
       </v-container>
@@ -62,35 +81,50 @@
     },
     data () {
       const defaultForm = Object.freeze({
-        description: '',
-        image: ''
+        username: '',
+        image: '',
+        email: '',
+        password: ''
       })
 
       return {
         form: Object.assign({}, defaultForm),
         rules: {
-          description: [val => (val || '').length > 0 || 'This field is required'],
+          username: [val => (val || '').length > 0 || 'This field is required'],
+          password: [val => (val || '').length > 0 || 'This field is required'],
+          emailRules: [ 
+            (v) => !!v || 'E-mail is required',
+            (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid' 
+          ],
           image: [val => (val || '').length > 0 || 'This field is required']
         },
         snackbar: false,
+        e1: false,
         userId: '',
         defaultForm
       }
     },
     created () {
       if (localStorage.getItem('token')) {
-        // let token = localStorage.getItem('token')
-        // let decode = jwt_decode(token)
-        // this.userId = decode.id
+        this.getProfileAccount()
+        console.log(this.profileAccount)
+        this.form.username = this.profileAccount.username
+        this.form.email = this.profileAccount.email
+        this.form.password = this.profileAccount.password
       }
     },
     computed: {
       formIsValid () {
         return (
-          this.form.description &&
+          this.form.username &&
+          this.form.email &&
+          this.form.password &&
           this.form.image
         )
-      }
+      },
+      ...mapState([
+        'profileAccount'
+      ])
     },
     methods: {
       resetForm () {
@@ -98,7 +132,8 @@
         this.$refs.form.reset()
       },
       ...mapActions([
-        'addPost'
+        'editProfile',
+        'getProfileAccount'
       ])
     }
   }
--- client/src/components/Profile-Head.vue
+++ client/src/components/Profile-Head.vue
@@ -8,7 +8,43 @@
     <v-flex md8 xs12>
       <v-card>
         <v-card-text>
-          <h1>{{profileAccount.username}}</h1>
+          <h1>{{profileAccount.username}} 
+          <small>
+            <v-dialog v-model="dialog" persistent max-width="500px">
+              <v-btn small color="primary" dark slot="activator">Edit Profile<v-icon right dark style="font-size:12px;">settings</v-icon></v-btn>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">Add Post</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container grid-list-md>
+                    <v-layout wrap>
+                      <v-flex xs12>
+                        <profile-edit-form></profile-edit-form>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                  <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="red" dark @click.native="dialog = false">Close</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </small>
+            <small>
+              <v-btn
+              small
+              @click="logout()"
+              color="blue-grey"
+              class="white--text"
+              >
+              Logout
+              <v-icon right dark style="font-size:12px;">lock</v-icon>
+            </v-btn>
+          </small>
+          </h1>
         </v-card-text>
         <v-card-text>
           <div id="post">
@@ -28,11 +64,16 @@
 
 <script>
 import { mapActions, mapState } from 'vuex'
+import ProfileEditForm from './Profile-Edit-Form'
 
 export default {
+  components: {
+    'profile-edit-form': ProfileEditForm
+  },
   data () {
     return {
-      show: false
+      show: false,
+      dialog: false
     }
   },
   created () {
@@ -47,7 +88,11 @@ export default {
       'getProfileTimeline',
       'getFollowing',
       'getFollowers'
-    ])
+    ]),
+    logout () {
+      localStorage.clear()
+      location.reload()
+    }
   },
   computed: {
     ...mapState([
@@ -70,5 +115,8 @@ export default {
   }
   #post #post-count {
     float: left;
+  }
+  small {
+    font-size: 13px;
   }
 </style>
